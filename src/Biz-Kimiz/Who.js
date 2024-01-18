4ab905904f6c90352feebcae817aaa4ec1a95957
alivehbi <alivehbi@sabanciuniv.edu>
2024-01-19 02:47:56 +0300

    BizKimiz Page Redesign

--- src/Biz-Kimiz/Who.js
+++ src/Biz-Kimiz/Who.js
@@ -9,16 +9,20 @@ import './Who.css'
 import Footer from '../Footer/Footer';
 import { useScrollToTop } from '../blog/useScrollToTop';
 import PersonCard from './PersonCard';
+import SezenImg from "../Images/SezenImg.jpg"
+import CaglaImg from "../Images/CaglaImg.jpg"
+
 const defaultTheme = createTheme();
 const persons = [
-    { name: 'Başar Baysal', image: 'https://pbs.twimg.com/profile_images/1687854148966883328/yXb4UhhZ_400x400.jpg', explanation: "Sitenin Sahibi" },
+    { name: 'Başar Baysal', image: 'https://pbs.twimg.com/profile_images/1687854148966883328/yXb4UhhZ_400x400.jpg', explanation: "Ekip Lideri" },
     { name: 'Ali Vehbi Güneysu', image: 'https://alivehbiguneysu.net/static/media/myPhoto.aec4a690d0b700cc2821.jpg', explanation: "Geliştirici" },
     { name: 'Arman Gökalp', image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg', explanation: "Geliştirici" },
-    { name: 'Çağla', image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg', explanation: "İçerik Sorumlusu" },
-    { name: 'Sezen', image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg', explanation: "İçerik Sorumlusu" },
+    { name: 'Çağla Vural', image: CaglaImg, explanation: "İçerik Sorumlusu" },
+    { name: 'Sezen Kaya Sönmez', image: SezenImg, explanation: "İçerik Sorumlusu" },
 
 ]
 
+const bizKimiz = "İklim değişikliği yüzyılımızın giderek çeşitlenen ekolojik sorunları içinde küresel boyutu ve yıkıcı etkileriyle öne çıkmaya başladı. İklim değişikliği; gıda güvenliğinden, yangınlar ya da seller gibi aşırı iklim olaylarına; su savaşları gibi azalan kaynaklar için silahlı çatışmalardan iklim temelli göç hareketlerine kadar birçok farklı güvenlik riski yaratma potansiyeline sahip. Bu tehditler hem devletleri, hem insanları, hem de gezegenimiz ve içerisinde barındırdığı eko-sistemi tehdit ediyor. Ülkemiz de iklim değişikliği etkilerini açısından yüksek riskli bölge olarak tanımlanan Doğu Akdeniz Havzası’nda bulunuyor ve iklim değişikliğini yaratacağı güvenlik tehditlerinden hiçbirisi bize uzak olan riskler değil. Bizler iklim değişikliğinin yaratacağı, hatta yaratmaya başladığı, bu yıkıcı riskler konusunda toplumun farkındalığını arttırmak için yola çıktık ve bu web sitesini oluşturduk. Temel amacımız son derece teknik ve akademik terimlerle dolu bir alan olan iklim değişikliği konusunu herkesin kolayca anlayacağı bir dille aktarıp bu konuda genel farkındalığı arttırabilmek. Büyük bir tehdit bizi bekliyor ama ne yazık ki bir çoğumuz bunun hakkında detaylıca bir bilgiye sahip değil. İşte biz de bunu dert ettik, tamamen apolitik bir motivasyonla bu konuda ne yapabiliriz diye düşünüp işe bu platform ile başlamaya karar verdik. Web sitemiz iklim değişikliğinin yaracağı güvenlik riskleri hakkında temel bir sınıflandırma ve her bir güvenlik riski ile ilgili genel bilgiler içiren metinleri bünyesinde barındırıyor. Orta vadede buna videolu anlatımları da eklemeyi hedefliyoruz. Ayrıca her an yeni gelişmelerin yaşandığı bu alanda güncel bilgilere ulaşılması amacıyla bölgesel ve küresel ölçekte konuya dair önemli platformlara ve güncel haberlere kolay erişiminin sağlanması da hedeflerimizden bir diğeridir. Bu bağlantılara ana sayfamızdan ulaşabilirsiniz. Uzun lafın kısası bizler iklim değişikliği ile dertlenen insanlar olarak bunu daha çok kişiye anlatalım ki beraber dertlenebilelim dedik :) Umarız başarılı oluruz… Önerileriniz için lütfen bize ulaşın."
 
 const Who = () => {
 
@@ -32,10 +36,18 @@ const Who = () => {
                 <main>
                     <div className='who-header'>
                         <h1>Biz Kimiz?</h1>
-                        <p>İklim Güvenliği ve Riskleri websitesini</p>
+                        <p>İklim değişikliği yüzyılımızın giderek çeşitlenen ekolojik sorunları içinde küresel boyutu ve yıkıcı etkileriyle öne çıkmaya başladı.</p>
+                        <p>İklim değişikliği; gıda güvenliğinden, yangınlar ya da seller gibi aşırı iklim olaylarına; su savaşları gibi azalan kaynaklar için silahlı çatışmalardan iklim temelli göç hareketlerine kadar birçok farklı güvenlik riski yaratma potansiyeline sahip. Bu tehditler hem devletleri, hem insanları, hem de gezegenimiz ve içerisinde barındırdığı eko-sistemi tehdit ediyor.</p>
+                        <p>Ülkemiz de iklim değişikliği etkilerini açısından yüksek riskli bölge olarak tanımlanan Doğu Akdeniz Havzası’nda bulunuyor ve iklim değişikliğini yaratacağı güvenlik tehditlerinden hiçbirisi bize uzak olan riskler değil.</p>
+                        <p><b>Bizler iklim değişikliğinin yaratacağı, hatta yaratmaya başladığı, bu yıkıcı riskler konusunda toplumun farkındalığını arttırmak için yola çıktık ve bu web sitesini oluşturduk.</b> </p>
+                        <p>Temel amacımız son derece teknik ve akademik terimlerle dolu bir alan olan iklim değişikliği konusunu herkesin kolayca anlayacağı bir dille aktarıp bu konuda genel farkındalığı arttırabilmek. Büyük bir tehdit bizi bekliyor ama ne yazık ki bir çoğumuz bunun hakkında detaylıca bir bilgiye sahip değil. İşte biz de bunu dert ettik, tamamen apolitik bir motivasyonla bu konuda ne yapabiliriz diye düşünüp işe bu platform ile başlamaya karar verdik.</p>
+                        <p>Web sitemiz iklim değişikliğinin yaracağı güvenlik riskleri hakkında temel bir sınıflandırma ve her bir güvenlik riski ile ilgili genel bilgiler içiren metinleri bünyesinde barındırıyor. Orta vadede buna videolu anlatımları da eklemeyi hedefliyoruz. Ayrıca her an yeni gelişmelerin yaşandığı bu alanda güncel bilgilere ulaşılması amacıyla bölgesel ve küresel ölçekte konuya dair önemli platformlara ve güncel haberlere kolay erişiminin sağlanması da hedeflerimizden bir diğeridir. Bu bağlantılara ana sayfamızdan ulaşabilirsiniz. </p>
+                        <p><b>Uzun lafın kısası bizler iklim değişikliği ile dertlenen insanlar olarak bunu daha çok kişiye anlatalım ki beraber dertlenebilelim dedik :) Umarız başarılı oluruz…</b></p>
+                        <p>Önerileriniz için lütfen bize ulaşın.</p>
                     </div>
                     <div className='who-main-div'>
                         <div className='who-divider'/>
+                        <h1 className='who-ekbimiz-title'>Ekibimiz</h1>
                         <div className='who-persons-container'>
                             <PersonCard person={persons[0]}/>
                         </div>
