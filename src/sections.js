a9c118cdb7a1a053db8d4f08fc642c2ef4369b3e
alivehbi <alivehbi@sabanciuniv.edu>
2024-01-19 04:01:46 +0300

    Main Page New Features
    [feature]: Users now can see how many links on the gallery and also they easily switch between them.

--- src/sections.js
+++ src/sections.js
@@ -1,8 +1,8 @@
 export const sections = [
-    { title: 'Extreme Climate Events', url: '/Extreme' },
-    { title: 'Food Insecurity', url: '/Food' },
-    { title: 'Water Insecurity', url: '/Water' },
-    { title: 'Economic Insecurity', url: '/Economic' },
-    { title: 'Climate Induced Migration', url: '/Migration' },
-    { title: 'Armed Conflict', url: '/Conflict' },
+    { title: 'Aşırı Hava Olayları', url: '/Extreme' },
+    { title: 'Gıda Güvenliği', url: '/Food' },
+    { title: 'Su Güvenliği', url: '/Water' },
+    { title: 'Ekonomi ve Enerji', url: '/Economic' },
+    { title: 'İklim Değişikliği Kaynaklı Göç', url: '/Migration' },
+    { title: 'Azalan Kaynaklar Sonucu Silahlı Çatışma', url: '/Conflict' },
 ];
